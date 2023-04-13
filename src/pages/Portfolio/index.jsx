4c7310e17764d9022b0836b2f848782e79a01933
dhdessoldi <dhdessoldi@hotmail.com>
2023-04-13 15:10:36 -0300

    post 1 finalizado

--- src/pages/Portfolio/index.jsx
+++ src/pages/Portfolio/index.jsx
@@ -1,13 +1,14 @@
 import styles from "./Portfolio.module.css";
 import posts from "json/posts.json";
 import PostCard from "components/PostCard";
+import { Link } from "react-router-dom";
 
 
 export default function Portfolio() {
   return (
     <section className={styles.container}>
       <h1 className={styles.container__title}>Um pouco sobre meu trabalho</h1>
-      <p className={styles.container__text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum faucibus nisl augue, a fringilla ipsum egestas pellentesque. Vestibulum accumsan feugiat porttitor. Proin vehicula quis ligula sed consequat. Sed sodales ut augue a venenatis. Ut venenatis fermentum tincidunt. Phasellus lorem leo, tincidunt id lacus id, placerat varius odio. Cras sollicitudin lectus non mollis condimentum.</p>
+      <p className={styles.container__text}>Aqui estão um pouco mais dos meus trabalhos como calculista e projetista de estruturas em aço. Separei os tipos mais usuais de estruturas no mercado e ressalto que as descrições são simplificadas e as imagens são apenas ilustrativas. Caso tenham alguma dúvida, ou queira saber mais, sinta-se à vontade de entrar em <Link to='/contato'>contato</Link>.</p>
     <ul className={styles.container__posts}>
       {posts.map((post) => (
         <li key={post.id}>
